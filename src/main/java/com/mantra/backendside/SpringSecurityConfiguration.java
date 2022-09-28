069b6c942b2d4a42bcf55171ecdf19424b4733e0
Marcin <shoshinmas@protonmail.com>
2022-09-28 14:21:10 +0200

    PreSQL - Spring Security implemented

---
+++ src/main/java/com/mantra/backendside/SpringSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.mantra.backendside;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        Function<String, String> encoder = input -> passwordEncoder().encode(input);
+
+        UserDetails user = User.builder().passwordEncoder(
+                        encoder
+                )
+                .username("mantra")
+                .password("dummy")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
