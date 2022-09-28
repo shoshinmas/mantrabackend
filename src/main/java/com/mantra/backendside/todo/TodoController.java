4c430c8bba1202a2695447a652164618231b7a1e
Marcin <shoshinmas@protonmail.com>
2022-09-28 13:01:49 +0200

    Update + date update added

--- src/main/java/com/mantra/backendside/todo/TodoController.java
+++ src/main/java/com/mantra/backendside/todo/TodoController.java
@@ -3,8 +3,10 @@ package com.mantra.backendside.todo;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,11 +41,41 @@ public class TodoController {
     }
 
     @RequestMapping(value="/user/add-todo", method = RequestMethod.POST)
-    public String addNewTodo(ModelMap model, Todo todo) {
+    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+       if(result.hasErrors()){
+           return "todo";
+       }
         String username = (String)model.get("name");
         todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
+
+    @RequestMapping(value="/user/delete-todo")
+    public String deleteTodo(@RequestParam int id) {
+        //Delete todo
+
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+
+    }
+    @RequestMapping(value="/user/update-todo", method = RequestMethod.GET)
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value="/user/update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+        if(result.hasErrors()){
+            return "todo";
+        }
+        String username = (String)model.get("name");
+        todo.setUsername(username);
+        todoService.updateTodo(todo);
+        return "redirect:list-todos";
+    }
+
 
 }
--- src/main/java/com/mantra/backendside/todo/TodoService.java
+++ src/main/java/com/mantra/backendside/todo/TodoService.java
@@ -3,7 +3,9 @@ package com.mantra.backendside.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,7 @@ public class TodoService {
     private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(++todosCount, "user","Learn AWS",
+        todos.add(new Todo(++todosCount, "user","Learn AWS and get certified",
                 LocalDate.now().plusYears(1), false ));
         todos.add(new Todo(++todosCount, "user","Learn DevOps",
                 LocalDate.now().plusYears(2), false ));
@@ -28,6 +30,23 @@ public class TodoService {
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
         Todo todo = new Todo(++todosCount,username,description,targetDate,done);
+        todos.add(todo);
+    }
+
+    public void deleteById(int id) {
+        //todo.getId() == id
+        // todo -> todo.getId() == id
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        return todos.stream().filter(predicate).findFirst().get();
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
         todos.add(todo);
     }
 }
