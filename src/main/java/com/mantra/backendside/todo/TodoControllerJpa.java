bf4e0968f1cf4ef3d80629c1d7acaad0d9a3712c
Marcin <shoshinmas@protonmail.com>
2022-10-07 16:55:00 +0200

    Function sendJsonTodo sends Json Todo :))

--- src/main/java/com/mantra/backendside/todo/TodoControllerJpa.java
+++ src/main/java/com/mantra/backendside/todo/TodoControllerJpa.java
@@ -1,6 +1,7 @@
 package com.mantra.backendside.todo;
 
 import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
